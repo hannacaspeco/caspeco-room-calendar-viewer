440be3384eb9ac7628afb7945658a76f03364600
Hanna Forssell <hanna.forssell@caspeco.se>
2024-03-05 08:27:15 +0100

    Redirect to page located at Caspeco

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,6 @@ import { Router } from "./Router";
 import { RouterProvider } from "react-router-dom";
 
 export default function App() {
+  window.location.replace("https://caspecoairdev.z16.web.core.windows.net/");
   return <RouterProvider router={Router} />;
 }
